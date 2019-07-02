1b488b997e18ac3162d7471b7117a0b21756e83c
Francis Mercier <Francis@nzt.ca>
2019-07-01 21:22:29 -0400

    Start-wactch batch file
    The batch file will launch the filesystem watchers. Start-watch, when launched from the batch file, will relaunch itself as administrator if needed (This is so App pool can be recycled).

--- Template/root/Start-Watch.ps1
+++ Template/root/Start-Watch.ps1
@@ -1,20 +1,36 @@
+Param([Switch]$Wait, $Path)
+$AdminMode = (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if ([String]::IsNullOrWhiteSpace($Path) -eq $false) {
+    if ($AdminMode -eq $false) {
+        $Args = @(('-File "{0}\Start-watch.ps1"' -f $Path), '-wait', ('-path "{0}\"' -f $Path))
+        Start-Process -FilePath Powershell.exe -WorkingDirectory $Path -ArgumentList $Args -Verb runas -ErrorAction Stop 
+        exit
+    }
+    Set-Location -Path $Path 
+}
 
 Import-Module DUDDashboard
+#import-module 'C:\Github\DudDashboard\DudDashboard.psd1' 
 $root = $PSScriptRoot
 Set-DUDSettingsCache -Path $root
 
 $UDHotParams = @{
     Root          = $root
     UpdateToken   = $cache:Settings.UDConfig.UpdateToken
-    DashboardPath = (Resolve-Path -Path $Appsettings.UDConfig.FilePath)
+    DashboardPath = (Resolve-Path -Path $cache:Settings.UDConfig.FilePath)
     Url           = $cache:Settings.HotReload.UpdateURL
     AppPool       = $cache:Settings.HotReload.AppPool
     UpdateDelay   = 750
 }
 
-#$Path1 = New-DUDHotReloaderWatchPath -Root (New-DUDHotReloaderWatchPathItem -Path 'C:\Users\xx\Documents\GitHub\SomeOtherThing' -Filter '*.*' -Recurse -Action Update)
+#$Path1 = New-DUDHotReloaderWatchPath -Root (New-DUDHotReloaderWatchPathItem -Path 'C:\Users\itfranck\Documents\GitHub\DUDDashboard' -Filter '*.*' -Recurse -Action Update)
 
 Start-DUDHotReloader @UDHotParams # -AdditionalPaths $Path1
 
+Write-Host $Path
 
-
+if ($Wait) {
+    while ($true) {
+        Start-Sleep -Milliseconds 200
+    }
+}
---
+++ Template/root/_Start-Watch.bat
@@ -0,0 +1,5 @@
+@setlocal enableextensions
+@cd /d "%~dp0"
+Powershell.exe -File "Start-Watch.ps1" -Path "%~dp0\" -wait
+
+
